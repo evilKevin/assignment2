f854ada2fd0bf4c48ac5189a053a406855944b9e
Connor Flaherty <ctflaherty9@gmail.com>
2017-09-07 12:17:05 -0500

    Add initial files

---
+++ assignment2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace assignment2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int option;
+
+            WriteLine("Enter 1, 2, or 3 to select your option, then press Enter: \n1. Calculate sum of scores\n 2. Enter customer purchases\n 3. Quit");
+            option = Convert.ToInt32(ReadLine());
+            WriteLine("Option number chosen: {0}", option);
+            ReadLine();
+
+
+            if (option == 1)
+            {
+
+            }
+            else if (option == 2)
+            {
+
+            }
+            else
+            {
+
+            }
+        }
+    }
+}
